085521847c643707a06acad2f4268e0678f60af4
PJSSABER <yiyu.pu@intel.com>
2024-01-11 23:23:37 +0800

    generic add

--- single_list/src/lib.rs
+++ single_list/src/lib.rs
@@ -20,7 +20,7 @@ impl<T> List<T> {
         let new_node = Box::new(Node {
             elem: elem,
             // next: self.head,     if use this, will occur an error!!  cannot move out of `self.head` which is behind a mutable reference, self.head will be dingling!!
-            next: self.head.take(), // this equals to mem::replace(&mut self.head, None)
+            next: self.head.take(), // this equals to mem::replace(&mut self.head, None), Takes the value out of the option, leaving a None in its place.
         });
 
         self.head = Some(new_node); 
@@ -28,9 +28,26 @@ impl<T> List<T> {
 
     pub fn pop(&mut self) -> Option<T> {
 
-        if let Some(node) = &mut self.head {
-            self.head = (*node).next.take();
+        if let Some(tmp_node) = self.head.take() {
+            self.head = tmp_node.next;
+            return Some(tmp_node.elem);
+        }
+        /*  using map: Maps an Option<T> to Option<U> by applying a function to a contained value (if Some) or returns None (if None).
+            self.head.take().map(|node| {
+                self.head = node.next;
+                node.elem
+            });
+        */
+        return None;
+    }
+}
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_node = self.head.take();
+        while let Some(mut node) = cur_node {
+            cur_node = node.next.take();
+            // node goes out of scope and calls drop automatically
         }
     }
 }

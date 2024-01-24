bec299f7e6bd7edf8637f345375752e8fa74759c
YY.PU <yiyu.pu@intel.com>
2024-01-24 23:31:58 +0800

    Update lib.rs

--- single_list/src/lib.rs
+++ single_list/src/lib.rs
@@ -20,7 +20,7 @@ impl<T> List<T> {
         let new_node = Box::new(Node {
             elem: elem,
             // next: self.head,     if use this, will occur an error!!  cannot move out of `self.head` which is behind a mutable reference, self.head will be dingling!!
-            next: self.head.take(), // this equals to mem::replace(&mut self.head, None)
+            next: self.head.take(), // this equals to mem::replace(&mut self.head, None), Takes the value out of the option, leaving a None in its place.
         });
 
         self.head = Some(new_node); 
@@ -28,9 +28,26 @@ impl<T> List<T> {
 
     pub fn pop(&mut self) -> Option<T> {
 
-        if let Some(node) = &mut self.head {
-            self.head = (*node).next.take();
+        if let Some(tmp_node) = self.head.take() {
+            self.head = tmp_node.next;
+            return Some(tmp_node.elem);
+        }
+        /*  using map: Maps an Option<T> to Option<U> by applying a function to a contained value (if Some) or returns None (if None).
+            self.head.take().map(|node| {
+                self.head = node.next;
+                node.elem
+            });
+        */
+        return None;
+    }
+}
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_node = self.head.take();  // Link<T>
+        while let Some(mut node) = cur_node { // Box<T> 类型是一个智能指针，因为它实现了 Deref trait，它允许 Box<T> 值被当作引用对待。当 Box<T> 值离开作用域时，由于 Box<T> 类型 Drop trait 的实现，box 所指向的堆数据也会被清除。
+            cur_node = node.next.take();
+            // node goes out of scope and calls drop automatically
         }
     }
 }
